2adb35e8c17e6ffdda4514c33088c05cdd66cf3d
Andresssg <andres-guevara1@upc.edu.co>
2023-02-20 21:31:38 -0500

    Modify lab to finish it

--- app/src/main/java/com/edu/unipiloto/mymessenger/CreateMessageActivity.java
+++ app/src/main/java/com/edu/unipiloto/mymessenger/CreateMessageActivity.java
@@ -1,25 +1,36 @@
 package com.edu.unipiloto.mymessenger;
 
+import static com.edu.unipiloto.mymessenger.ReceiveMessageActivity.EXTRA_MESSAGE;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class CreateMessageActivity extends AppCompatActivity {
+
+    private String history = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_message);
+        Intent intent = getIntent();
+        String messageText = intent.getStringExtra(EXTRA_MESSAGE);
+        TextView messageView = (TextView) findViewById(R.id.messages);
+        history = messageText != null ? messageText : "" + "\n";
+        messageView.setText(messageText);
     }
 
     public void onSendMessage(View view) {
         EditText messageView = (EditText) findViewById(R.id.message);
         String messageText = messageView.getText().toString();
+        String toSend = history + messageText;
         Intent intent = new Intent(this, ReceiveMessageActivity.class);
-        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
+        intent.putExtra(EXTRA_MESSAGE, toSend);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/edu/unipiloto/mymessenger/ReceiveMessageActivity.java
+++ app/src/main/java/com/edu/unipiloto/mymessenger/ReceiveMessageActivity.java
@@ -4,20 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ReceiveMessageActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "message";
-
+    private String history = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_message);
         Intent intent = getIntent();
         String messageText = intent.getStringExtra(EXTRA_MESSAGE);
-        TextView messageView = (TextView) findViewById(R.id.message);
+        TextView messageView = (TextView) findViewById(R.id.messages);
+        System.out.println(messageText + " antes");
+        history = messageText + "\n";
+        System.out.println(history);
         messageView.setText(messageText);
+    }
 
+    public void onSendMessage(View view) {
+        EditText messageView = (EditText) findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+        String toSend = history+ messageText;
+        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, toSend);
+        startActivity(intent);
     }
 }
